b817767eb9d01291db24f75acfbcac237ac8082d
zqbAse <1211455492@qq.com>
2022-03-11 16:45:00 +0800

    Update Engine.h

--- Poker/Engine.h
+++ Poker/Engine.h
@@ -8,14 +8,13 @@ class Engine {
 public:
 	Engine() { }
 
-	unsigned char Maxstrength(int hand, int community) {		//找最大hand strength
-		unsigned char strength = 100;
+	unsigned char Maxstrength(int hand, int community) {		
 		if ((hand % 3) == (community % 3))
-			return (hand % 3) + 3;
+			return (hand % 3) + 6;
 		else
-			return (hand % 3) > (community % 3) ? (hand % 3) : (community % 3);
+			return (hand % 3) + (community % 3);
 	}
-	void compute_winner(LeducPokerTable& table) {//计算玩家输赢
+	void compute_winner(LeducPokerTable& table) {
 
 		int strength1 = Maxstrength(table.players[0].cards.eval_card, table.community_cards.eval_card);
 		int strength2 = Maxstrength(table.players[1].cards.eval_card, table.community_cards.eval_card);
